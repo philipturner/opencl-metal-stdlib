9d5bc54ba55cbfb3450efd23bba252c0e7550cfc
Philip Turner <philipturner.AR@gmail.com>
2023-02-26 21:28:38 -0500

    Update Tests.swift

--- Tests.swift
+++ Tests.swift
@@ -38,7 +38,91 @@ if let headersDirectoryFlagIndex = CommandLine.arguments.firstIndex(
 } else {
   guard let _headerURL = Bundle.main.url(
     forResource: "metal_stdlib", withExtension: "h") else {
-    fatalError("Could not find locate header in Xcode app bundle.")
+    fatalError("Could not locate header in Xcode app bundle.")
   }
   headerURL = _headerURL
 }
+guard let headerData = FileManager.default.contents(
+  atPath: headerURL.absoluteString) else {
+  fatalError("Invalid header path: \(headerURL.absoluteString)")
+}
+guard let headerString = String(data: headerData, encoding: .utf8) else {
+  fatalError("Malformatted header: \(headerURL.absoluteString)")
+}
+
+var defaultTypes = ["char", "short", "int", "long"]
+defaultTypes += defaultTypes.map { "u" + $0 }
+defaultTypes.append("float")
+
+protocol ShaderRepresentable {
+  static var shaderType: String { get }
+}
+
+extension Bool: ShaderRepresentable { static let shaderType = "bool" }
+extension Int8: ShaderRepresentable { static let shaderType = "char" }
+extension Int16: ShaderRepresentable { static let shaderType = "short" }
+extension Int32: ShaderRepresentable { static let shaderType = "int" }
+extension Int64: ShaderRepresentable { static let shaderType = "long" }
+extension UInt8: ShaderRepresentable { static let shaderType = "uchar" }
+extension UInt16: ShaderRepresentable { static let shaderType = "ushort" }
+extension UInt32: ShaderRepresentable { static let shaderType = "uint" }
+extension UInt64: ShaderRepresentable { static let shaderType = "ulong" }
+extension Float: ShaderRepresentable { static let shaderType = "float" }
+
+extension SIMD2: ShaderRepresentable where Scalar: ShaderRepresentable {
+  static var shaderType: String {
+    "\(Scalar.shaderType)2"
+  }
+}
+extension SIMD3: ShaderRepresentable where Scalar: ShaderRepresentable {
+  static var shaderType: String {
+    "\(Scalar.shaderType)3"
+  }
+}
+extension SIMD4: ShaderRepresentable where Scalar: ShaderRepresentable {
+  static var shaderType: String {
+    "\(Scalar.shaderType)4"
+  }
+}
+
+// Use `auto` keyword to make the body type-generic.
+// Returns a dictionary matching types to their respective kernel.
+func generateKernels(
+  body: String, types: [String] = defaultTypes
+) -> [String: String] {
+  var output: [String: String] = [:]
+  for type in types {
+    output[type] = """
+    #if __METAL__
+    #include <metal_stdlib>
+    using namespace metal;
+    
+    #define KERNEL kernel
+    #define GLOBAL device
+    #define BUFFER_BINDING(index) [[buffer(index)]]
+    #else
+    \(headerString)
+    
+    #define KERNEL __kernel
+    #define GLOBAL __global
+    #define BUFFER_BINDING(index)
+    #endif
+    
+    KERNEL void vectorOperation(
+      GLOBAL \(type)* a BUFFER_BINDING(0),
+      GLOBAL \(type)* b BUFFER_BINDING(1),
+      GLOBAL \(type)* c BUFFER_BINDING(2)
+    #if __METAL__
+      , uint tid [[thread_position_in_grid]]
+    ) {
+    #else
+    ) {
+      uint tid = uint(get_global_id(0));
+    #endif
+      \(body)
+    }
+    """
+  }
+}
+
+// TODO: Make common protocol to unify code for Metal and OpenCL
